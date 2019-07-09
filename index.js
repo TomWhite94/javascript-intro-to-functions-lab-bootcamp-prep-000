a105e3c5720e1b71b5f54a8346aa54cbe5565eee
Tom White <tomjameswhite94@gmail.com>
2019-07-09 11:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (shout(string) === 'HELLO') {
+  if (shout === 'HELLO') {
   console.log("YES INDEED!")
   }
 }
