2e665029600bfc6e4f5bd3210f4ddb34efc2682f
Tom White <tomjameswhite94@gmail.com>
2019-07-09 11:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (shout === 'HELLO') {
+  if (shout === toUpperCase()) {
   console.log("YES INDEED!")
   }
 }
