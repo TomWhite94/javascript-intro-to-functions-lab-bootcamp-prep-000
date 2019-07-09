258fe5ae079d1ad7a64d3432d854967e753c3c89
Tom White <tomjameswhite94@gmail.com>
2019-07-09 11:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string) {
 }
 function sayHiToGrandma (string) {
  if (logWhisper)
- return ('I can\'t hear you!');
- if (logShout)
  return ('YES INDEED!');
+ if (logShout)
+ return ('I can\'t hear you!');
  if ('I love you, Grandma.')
  return ('I love you, too.');
 }
